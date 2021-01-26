81d51aed338a33235b50bf71eaeb8a62046b32d1
Balasubramanian, Saravanan <saravanan.b@hotmail.com>
2021-01-25 22:50:14 -0600

    Create CustomerController.cs

---
+++ CustomerService/CustomerService/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using CustomerService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerController(ICustomerRepository repository)
+        {
+            _customerRepository = repository;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Customer> Get(int id)
+        {
+            var customer = _customerRepository.GetCustomer(id);
+
+            if (customer == null)
+                return NotFound();
+
+            return customer;
+        }
+
+    }
+}
