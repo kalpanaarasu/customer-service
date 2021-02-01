85537aed8e464f380e604df5528aec718e80a4ab
kalpanaarasu <34888104+kalpanaarasu@users.noreply.github.com>
2021-02-01 16:05:49 +0530

    nothing important

--- CustomerService/CustomerService/Startup.cs
+++ CustomerService/CustomerService/Startup.cs
@@ -27,10 +27,16 @@ namespace CustomerService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddDbContextPool<AppDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("CustomerDBConnection")));
             services.AddScoped<ICustomerRepository, SQLCustomerRepository>();
             services.AddControllers();
-
+            services.AddCors();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins",
+                    builder => builder.AllowAnyOrigin());
+            });
             services.AddSwaggerGen();
 
         }
@@ -38,6 +44,10 @@ namespace CustomerService
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(builder =>
+          builder.WithOrigins("http://localhost:4200", "http://www.myclientserver.com")
+              .AllowAnyHeader()
+              .AllowAnyMethod());
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
 
